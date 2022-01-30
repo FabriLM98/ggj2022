2333ede92acfea2eda8572787944b71a279504e2
FabriLM98 <63129784+FabriLM98@users.noreply.github.com>
2022-01-30 01:22:12 -0300

    assets

---
+++ GGJ2022/Assets/Scripts/Pato/PatoEstado.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PatoEstado
+{
+    public abstract void AnteTrigger(Collider2D collision);
+    public abstract bool EstaDebajoDelAgua();
+    public abstract void EntradoEstado(Pato pato);
+    public abstract void UpdateFrame();
+    public abstract void PatoApretado();
+    public abstract void PatoArrastrado();
+    public abstract void PatoSoltado();
+}
---
+++ GGJ2022/Assets/Scripts/PezManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PezManager : MonoBehaviour
+{
+    [SerializeField] GameObject pezPrefab;
+    float timer = 0;
+    [SerializeField] float timeBetweenSpawn = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > timeBetweenSpawn)
+        {
+            timer = 0;
+            HacerAparecerPez();
+        }
+    }
+    void HacerAparecerPez()
+    {
+        GameObject instancia = Instantiate(pezPrefab);
+        instancia.transform.position = new Vector3(Random.Range(-2.3f, 2.3f), this.transform.position.y, 0);
+    }
+}
